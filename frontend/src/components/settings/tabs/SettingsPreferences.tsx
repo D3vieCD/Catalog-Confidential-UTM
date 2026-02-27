d97cdbe7aec1a1d19363a36c20018cb12e7a9f5e
Dascal_Sergiu <sergiu.dascal@isa.utm.md>
2026-02-27 16:08:43 +0200

    Definirea proprietati pentru componenta SettingsPreferences

--- frontend/src/components/settings/tabs/SettingsPreferences.tsx
+++ frontend/src/components/settings/tabs/SettingsPreferences.tsx
@@ -2,3 +2,7 @@ interface PreferencesData {
   language: string;
   timezone: string;
 }
+interface SettingsPreferencesProps {
+  preferences: PreferencesData;
+  setPreferences: (preferences: PreferencesData) => void;
+}
