cd753c3764c3db2acbc18fa9394b36fd421ff6c4
Dascal_Sergiu <sergiu.dascal@isa.utm.md>
2026-03-02 22:10:23 +0200

    Adaugă comentariul și structura de bază pentru AnimatedElements

---
+++ frontend/src/components/sections/login/AnimatedElements.tsx
@@ -0,0 +1,8 @@
+/**
+ * Animated Elements - Elemente animate pentru gradient background
+ * Forme geometrice, cod binar, simboluri matematice
+ */
+
+export const AnimatedElements = () => {
+  return (
+    <></>
