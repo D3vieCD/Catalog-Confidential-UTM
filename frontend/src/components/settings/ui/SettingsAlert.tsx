a5d83efd807d5538019de175b7fa3542aac00d83
Dascal_Sergiu <sergiu.dascal@isa.utm.md>
2026-03-02 19:53:42 +0200

    Crearea structura componenta SettingsAlert

--- frontend/src/components/settings/ui/SettingsAlert.tsx
+++ frontend/src/components/settings/ui/SettingsAlert.tsx
@@ -3,3 +3,15 @@ interface SettingsAlertProps {
   title: string;
   message: string;
 }
+import React from 'react';
+
+export const SettingsAlert: React.FC<SettingsAlertProps> = ({
+  type,
+  title,
+  message
+}) => {
+  return (
+    <div>
+    </div>
+  );
+};
