f7cea763d9402606db2cb1debf2365a0e38a2a04
Cortac_Nicolae <cortac.nicolae777@gmail.com>
2026-03-02 22:09:34 +0200

    eroare 404

---
+++ frontend/src/pages/error/Page404.tsx
@@ -0,0 +1,11 @@
+import { paths } from '../../routes/paths';
+import { ErrorPage } from './ErrorPage';
+
+export const Page404 = () => (
+  <ErrorPage
+    code={404}
+    title="Pagina nu a fost găsită!"
+    description="Pagina pe care o cauți nu există sau a fost mutată."
+    navigateTo={paths.dashboard}
+  />
+);
