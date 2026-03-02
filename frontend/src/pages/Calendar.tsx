9f0013cdd059816b4045cbcb1bf07a5e7bab3b38
Ion Ripa <ionutripa12@gmail.com>
2026-03-02 12:39:36 -0600

    inițializare pagină Calendar și importuri componente

---
+++ frontend/src/pages/Calendar.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { CalendarHeader } from '../components/calendar/CalendarHeader';
+import { CalendarGrid } from '../components/calendar/CalendarGrid';
+import { CalendarSidebar } from '../components/calendar/CalendarSidebar';
+import { Modal } from '../components/ui/Modal';
+import { mockCalendarEvents, type CalendarEvent } from '../_mock/mockCalendar';
+
+export const Calendar = () => {
